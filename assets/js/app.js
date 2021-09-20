552b82c738ae85ed8589f7df93dba16e023f4be3
EFerri222 <eferri222@gmail.com>
2021-09-20 02:13:09 -0400

    Change round data and restructure lost to sentence; fix typo on Something Smells in dropdown, add lost data to MuscleBob BuffPants

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,12 @@ var episodes = {
     "arrgh!": {
         "lost": "Fools in April",
         "season": 1,
-        "round": 10
+        "round": "2nd round"
     },
     "artist unknown": {
         "lost": "Shanghaied",
         "season": 2,
-        "round": 30,
+        "round": "3rd round",
         "beat": ["I'm Your Biggest Fanatic"]
     },
     "band geeks": {
@@ -28,102 +28,102 @@ var episodes = {
     "big pink loser": {
         "lost": "Sailor Mouth",
         "season": 2,
-        "round": 29,
+        "round": "3rd round",
         "beat": ["No Free Rides"]
     },
     "boating school": {
         "lost": "Hall Monitor",
         "season": 1,
-        "round": 32,
+        "round": "3rd round",
         "beat": ["Mermaid Man and Barnacle Boy"]
     },
     "bossy boots": {
         "lost": "Graveyard Shift",
         "season": 2,
-        "round": 10
+        "round": "2nd round"
     },
     "bubble buddy": {
         "lost": "Imitation Krabs",
         "season": 2,
-        "round": 14
+        "round": "2nd round"
     },
     "bubblestand": {
         "lost": "Pizza Delivery",
         "season": 1,
-        "round": 34,
+        "round": "Quarter-Finals",
         "beat": ["Squeaky Boots", "Fools in April"]
     },
     "the chaperone": {
         "lost": "Jellyfishing",
         "season": 1,
-        "round": 6,
+        "round": "1st round"
     },
     "christmas who?": {
         "lost": "Dying for Pie",
         "season": 2,
-        "round": 33,
+        "round": "Quarter-Finals",
         "beat": ["The Smoking Peanut", "Imitation Krabs"]
     },
     "culture shock": {
         "lost": "Employee of the Month",
         "season": 1,
-        "round": 36,
+        "round": "Quarter-Finals",
         "beat": ["Sleepy Time", "Jellyfishing"]
     },
     "dumped": {
         "lost": "The Smoking Peanut",
         "season": 2,
-        "round": 5
+        "round": "1st round"
     },
     "dying for pie": {
         "lost": "Band Geeks",
-        "season": "2 Semi-Finals",
-        "round": 1,
+        "season": 2,
+        "round": "Semi-Finals",
         "beat": ["Survival of the Idiots", "Krusty Love", "Christmas Who?"]
     },
     "employee of the month": {
         "lost": "Hall Monitor",
-        "season": "1 Semi-Finals",
-        "round": 2,
+        "season": 1,
+        "round": "Semi-Finals",
         "beat": ["Plankton!", "F.U.N.", "Naughty Nautical Neighbors", "Culture Shock"]
     },
     "fools in april": {
         "lost": "Bubblestand",
         "season": 1,
-        "round": 26,
+        "round": "3rd round",
         "beat": ["Sandy's Rocket", "Arrgh!"]
     },
     "frankendoodle": {
         "lost": "Squidville",
         "season": 2,
-        "round": 28,
+        "round": "3rd round",
         "beat": ["Squid's Day Off"]
     },
     "the fry cook games": {
         "lost": "Sailor Mouth",
         "season": 2,
-        "round": 19
+        "round": "2nd round"
     },
     "f.u.n.": {
         "lost": "Employee of the Month",
         "season": 1,
-        "round": 20
+        "round": "2nd round"
     },
     "gary takes a bath": {
         "lost": "Graveyard Shift",
         "season": 2,
-        "round": 25,
+        "round": "3rd round",
         "beat": ["I'm With Stupid", "Mermaid Man and Barnacle Boy III"]
     },
     "grandma's kisses": {
         "lost": "No Free Rides",
         "season": 2,
-        "round": 7
+        "round": "1st round"
     },
     "graveyard shift": {
         "lost": "Band Geeks",
         "season": 2,
-        "round": 32,
+        "round": "Quarter-Finals",
         "beat": ["Bossy Boots", "Gary Takes a Bath"]
     },
     "hall monitor": {
@@ -135,142 +135,141 @@ var episodes = {
     "help wanted": {
         "lost": "SB-129",
         "season": 1,
-        "round": 35,
+        "round": "Quarter-Finals",
         "beat": ["Home Sweet Pineapple", "Tea at the Treedome"]
     },
     "home sweet pineapple": {
         "lost": "Help Wanted",
         "season": 1,
-        "round": 14
+        "round": "2nd round"
     },
     "hooky": {
         "lost": "SB-129",
         "season": 1,
-        "round": 16
+        "round": "2nd round"
     },
     "i was a teenage gary": {
         "lost": "Reef Blower",
         "season": 1,
-        "round": 5
+        "round": "1st round"
     },
     "i'm with stupid": {
         "lost": "Gary Takes a Bath",
         "season": 2,
-        "round": 3,
+        "round": "1st round"
     },
     "i'm your biggest fanatic": {
         "lost": "Artist Unknown",
         "season": 2,
-        "round": 20,
+        "round": "2nd round",
         "beat": ["Sandy, SpongeBob, and the Worm"]
     },
     "imitation krabs": {
         "lost": "Cristmas Who?",
         "season": 2,
-        "round": 27,
+        "round": "3rd round",
         "beat": ["Bubble Buddy"]
     },
     "jellyfish hunter": {
         "lost": "Life of Crime",
         "season": 2,
-        "round": 23,
-        "beat": [""]
+        "round": "2nd round"
     },
     "jellyfish jam": {
         "lost": "Texas",
         "season": 1,
-        "round": 33,
+        "round": "3rd round",
         "beat": ["Opposite Day"]
     },
     "jellyfishing": {
         "lost": "Culture Shock",
         "season": 1,
-        "round": 30,
+        "round": "3rd round",
         "beat": ["The Chaperone", "Mermaid Man and Barnacle Boy II"]
     },
     "karate choppers": {
         "lost": "SB-129",
         "season": 1,
-        "round": 29,
+        "round": "3rd round",
         "beat": ["Reef Blower"]
     },
     "krusty love": {
         "lost": "Dying for Pie",
         "season": 2,
-        "round": 26,
+        "round": "3rd round",
         "beat": ["Squirrel Jokes"]
     },
     "life of crime": {
         "lost": "Patty Hype",
         "season": 2,
-        "round": 31,
+        "round": "3rd round",
         "beat": ["Jellyfish Hunter"]
     },
     "mermaid man and barnacle boy": {
         "lost": "Boating School",
         "season": 1,
-        "round": 23
+        "round": "2nd round"
     },
     "mermaid man and barnacle boy ii": {
         "lost": "Jellyfishing",
         "season": 1,
-        "round": 18
+        "round": "2nd round"
     },
     "mermaid man and barnacle boy iii": {
         "lost": "Gary Takes a Bath",
         "season": 2,
-        "round": 1
+        "round": "2nd round"
     },
     "musclebob buffpants": {
-        "lost": "",
-        "season": null,
-        "round": null
+        "lost": "Nature Pants",
+        "season": 1,
+        "round": "1st round"
     },
     "nature pants": {
         "lost": "Hall Monitor",
         "season": 1,
-        "round": 22,
+        "round": "2nd round",
         "beat": ["MuscleBob BuffPants"]
     },
     "naughty nautical neighbors": {
         "lost": "Employee of the Month",
         "season": 1,
-        "round": 31,
+        "round": "3rd round",
         "beat": ["Suds"]
     },
     "neptune's spatula": {
         "lost": "Pizza Delivery",
         "season": 1,
-        "round": 13,
+        "round": "2nd round",
         "beat": ["The Paper"]
     },
     "no free rides": {
         "lost": "Big Pink Loser",
         "season": 2,
-        "round": 18,
+        "round": "2nd round",
         "beat": ["Grandma's Kisses"]
     },
     "opposite day": {
         "lost": "Jellyfish Jam",
         "season": 1,
-        "round": 24,
+        "round": "2nd round",
         "beat": ["Squidward the Unfriendly Ghost"]
     },
     "the paper": {
         "lost": "Neptune's Spatula",
         "season": 1,
-        "round": 3,
+        "round": "1st round"
     },
     "patty hype": {
         "lost": "Shanghaied",
         "season": 2,
-        "round": 35,
+        "round": "Quarter-Finals",
         "beat": ["Prehibernation Week", "Life of Crime"]
     },
     "pickles": {
         "lost": "Texas",
         "season": 1,
-        "round": 25
+        "round": "2nd round"
     },
     "pizza delivery": {
         "lost": "",
@@ -281,40 +280,40 @@ var episodes = {
     "plankton!": {
         "lost": "Employee of the Month",
         "season": 1,
-        "round": 7
+        "round": "1st round"
     },
     "prehibernation week": {
         "lost": "Patty Hype",
         "season": 2,
-        "round": 22
+        "round": "2nd round"
     },
     "pressure": {
         "lost": "Survival of the Idiots",
         "season": 2,
-        "round": 4
+        "round": "1st round"
     },
     "procrastination": {
         "lost": "Band Geeks",
         "season": 2,
-        "round": 9,
+        "round": "2nd round",
         "beat": ["Wormy"]
     },
     "reef blower": {
         "lost": "Karate Choppers",
         "season": 1,
-        "round": 17,
+        "round": "2nd round",
         "beat": ["I Was a Teenage Gary"]
     },
     "ripped pants": {
         "lost": "Pizza Delivery",
         "season": 1,
-        "round": 27,
+        "round": "3rd round",
         "beat": ["Rock Bottom"]
     },
     "rock bottom": {
         "lost": "Ripped Pants",
         "season": 1,
-        "round": 12
+        "round": "2nd round"
     },
     "sailor mouth": {
         "lost": "",
@@ -325,29 +324,29 @@ var episodes = {
     "sandy's rocket": {
         "lost": "Fools in April",
         "season": 1,
-        "round": 1
+        "round": "1st round"
     },
     "sandy, spongebob, and the worm": {
         "lost": "I'm Your Biggest Fanatic",
         "season": 2,
-        "round": 8
+        "round": "1st round"
     },
     "sb-129": {
         "lost": "Pizza Delivery",
-        "season": "1 Semi-Finals",
-        "round": 1,
+        "season": 1,
+        "round": "Semi-Finals",
         "beat": ["Hooky", "Karate Choppers", "Help Wanted"]
     },
     "scaredy pants": {
         "lost": "Tea at the Treedome",
         "season": 1,
-        "round": 15,
+        "round": "2nd round",
         "beat": ["Walking Small"]
     },
     "the secret box": {
         "lost": "Something Smells",
         "season": 2,
-        "round": 1,
+        "round": "2nd round",
     },
     "shanghaied": {
         "lost": "",
@@ -358,99 +357,99 @@ var episodes = {
     "sleepy time": {
         "lost": "Culture Shock",
         "season": 1,
-        "round": 19
+        "round": "2nd round"
     },
     "the smoking peanut": {
         "lost": "Christmas Who?",
         "season": 2,
-        "round": 15,
+        "round": "2nd round",
         "beat": ["Dumped (HAH!)"]
     },
     "something smells": {
         "lost": "Band Geeks",
         "season": 2,
-        "round": 24,
+        "round": "3rd round",
         "beat": ["The Secret Box"]
     },
     "squeaky boots": {
         "lost": "Bubblestand",
         "season": 1,
-        "round": 11,
+        "round": "2nd round",
         "beat": ["Valentine's Day"]
     },
     "squid on strike": {
         "lost": "Squidville",
         "season": 2,
-        "round": 6
+        "round": "1st round"
     },
     "squid's day off": {
         "lost": "Frankendoodle",
         "season": 2,
-        "round": 17
+        "round": "2nd round"
     },
     "squidville": {
         "lost": "Sailor Mouth",
         "season": 2,
-        "round": 34,
+        "round": "Quarter-Finals",
         "beat": ["Squid on Strike", "Welcome to the Chum Bucket", "Frankendoodle"]
     },
     "squidward the unfriendly ghost": {
         "lost": "Opposite Day",
         "season": 1,
-        "round": 9
+        "round": "1st round"
     },
     "squirrel jokes": {
         "lost": "Krusty Love",
         "season": 2,
-        "round": 12
+        "round": "2nd round"
     },
     "suds": {
         "lost": "Naughty Nautical Neighbors",
         "season": 1,
-        "round": 21
+        "round": "2nd round"
     },
     "survival of the idiots": {
         "lost": "Dying for Pie",
         "season": 2,
-        "round": 13,
+        "round": "2nd round",
         "beat": ["Pressure"]
     },
     "tea at the treedome": {
         "lost": "Help Wanted",
         "season": 1,
-        "round": 28,
+        "round": "3rd round",
         "beat": ["Scaredy Pants"]
     },
     "texas": {
         "lost": "Hall Monitor",
         "season": 1,
-        "round": 37,
+        "round": "Quarter-Finals",
         "beat": ["Pickles", "Jellyfish Jam"]
     },
     "valentine's day": {
         "lost": "Squeaky Boots",
         "season": 1,
-        "round": 2
+        "round": "1st round"
     },
     "walking small": {
         "lost": "Scaredy Pants",
         "season": 1,
-        "round": 4
+        "round": "1st round"
     },
     "welcome to the chum bucket": {
         "lost": "Squidville",
         "season": 2,
-        "round": 16
+        "round": "2nd round"
     },
     "wormy": {
         "lost": "Procrastination",
         "season": 2,
-        "round": 2,
+        "round": "1st round",
     },
     "your shoe's untied": {
         "lost": "Shanghaied",
         "season": 2,
-        "round": 21
+        "round": "2nd round"
     }
 };
 
@@ -476,7 +475,7 @@ submitBtn.onclick = (function(event){
             if(episodes[dropdown.value]["lost"] == "") {
                 lostResult.innerHTML = "This episode has not lost yet!";
             } else {
-                lostResult.innerHTML = `Lost to "${episodes[dropdown.value]["lost"]}" in Season ${episodes[dropdown.value]["season"]}: Round ${episodes[dropdown.value]["round"]}.`;
+                lostResult.innerHTML = `Lost to "${episodes[dropdown.value.toLowerCase()]["lost"]}" in the ${episodes[dropdown.value.toLowerCase()]["round"]} of Season ${episodes[dropdown.value.toLowerCase()]["season"]}.`;
             }
             // Handle beat list
             if(episodes[dropdown.value]["beat"]){
@@ -498,7 +497,7 @@ submitBtn.onclick = (function(event){
             if(episodes[textInput.value.toLowerCase()]["lost"] == "") {
                 lostResult.innerHTML = "This episode has not lost yet!";
             } else {
-                lostResult.innerHTML = `Lost to "${episodes[textInput.value.toLowerCase()]["lost"]}" in Season ${episodes[textInput.value.toLowerCase()]["season"]}: Round ${episodes[textInput.value.toLowerCase()]["round"]}.`;
+                lostResult.innerHTML = `Lost to "${episodes[textInput.value.toLowerCase()]["lost"]}" in the ${episodes[textInput.value.toLowerCase()]["round"]} of Season ${episodes[textInput.value.toLowerCase()]["season"]}.`;
             }
             // Handle beat list
             if(episodes[textInput.value.toLowerCase()]["beat"]){
